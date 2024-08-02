f4e8d5a800ea6cf953bb54853ddb9def733f5fe7
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-02 00:58:16 -0300

    chore: add tw color and width extension to themes and some code prettifying

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,13 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <main class="text-3xl font-bold underline">
-      <h1>{{ title }}</h1>
-      <router-outlet />
-    </main>`,
-  styles: ``,
+  template: ` <router-outlet />`,
 })
-export class AppComponent {
-  title = 'url-shortener';
-}
+export class AppComponent {}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
+      colors: {
+        primary: '#384078',
+        secondary: '#A6B1E1',
+        tertiary: '#DCD6F7',
+        success: '#387855',
+        error: '#b92a2a',
+      },
+      width: {
+        18: '4.5rem' /* 72px */,
+      },
       height: {
         18: '4.5rem' /* 72px */,
       },
