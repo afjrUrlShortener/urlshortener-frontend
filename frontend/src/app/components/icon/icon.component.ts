cb2a5fb42b185b1c3b8588c5108b5ed1506ba76d
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-02 22:20:04 -0300

    feat: add icon component for svgs

---
+++ frontend/src/app/components/icon/icon.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input } from '@angular/core';
+import { TIconName, TSize } from '../../types/common.type';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-icon',
+  standalone: true,
+  imports: [NgClass],
+  template: `
+    <svg
+      [ngClass]="{
+        'size-6': size === 'sm',
+        'size-12': size === 'md',
+        'size-24': size === 'lg',
+        'size-48': size === 'xlg',
+      }"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5">
+      @switch (name) {
+        @case ('home') {
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        }
+        @case ('bars-4') {
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+        }
+        @case ('hand-raised') {
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002" />
+        }
+        @case ('arrow-right') {
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        }
+        @case ('link') {
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+        }
+      }
+    </svg>
+  `,
+})
+export class IconComponent {
+  @Input({ required: true }) name: TIconName = '';
+  @Input() size: TSize = 'sm';
+}
