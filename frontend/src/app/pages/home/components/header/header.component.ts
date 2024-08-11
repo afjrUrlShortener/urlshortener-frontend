369ad948d7d89ee82d0f6106bd369288408ebab4
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-11 19:27:24 -0300

    feat!: remove unneceessary header components and move to header component

--- frontend/src/app/pages/home/components/header/header.component.ts
+++ frontend/src/app/pages/home/components/header/header.component.ts
@@ -1,23 +1,43 @@
 import { Component } from '@angular/core';
-import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
-import { THeaderMenuItem } from './types/header.types';
+import {
+  LinkComponent,
+  TLinkProps,
+} from '../../../../components/link/link.component';
 
 @Component({
   selector: 'home-header',
   standalone: true,
-  imports: [HeaderMenuComponent],
+  imports: [LinkComponent],
   template: `
-    <header class="bg-secondary p-4">
-      <nav>
-        <header-menu [headerMenuItems]="headerMenuItems" />
-      </nav>
+    <header class="flex items-center justify-between bg-secondary px-4 pt-4">
+      <app-link [props]="appLinkProps" />
+      <ul class="flex flex-grow items-center justify-evenly">
+        @for (linkProps of headerMenuProps; track linkProps.link) {
+          <li>
+            <app-link [props]="linkProps" />
+          </li>
+        }
+      </ul>
     </header>
   `,
 })
 export class HeaderComponent {
-  headerMenuItems: THeaderMenuItem[] = [
-    { text: 'Home', iconName: 'chevron-down' },
-    { text: 'About', iconName: 'chevron-down' },
-    { text: 'Pricing', iconName: 'chevron-down' },
+  appLinkProps: TLinkProps = {
+    link: '/',
+    underlineColor: 'primary',
+    icon: { name: '', size: 'md', color: 'primary' },
+  };
+
+  headerMenuProps: TLinkProps[] = [
+    {
+      link: 'about',
+      underlineColor: 'primary',
+      typography: { text: 'About' },
+    },
+    {
+      link: '404',
+      underlineColor: 'primary',
+      typography: { text: 'Not Found' },
+    },
   ];
 }
