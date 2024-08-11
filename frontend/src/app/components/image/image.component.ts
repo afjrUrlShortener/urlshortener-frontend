254efdfebbcb296d66a5073aec41a3afb1a93563
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-11 19:15:11 -0300

    feat!: modify image component to use prop type

--- frontend/src/app/components/image/image.component.ts
+++ frontend/src/app/components/image/image.component.ts
@@ -2,31 +2,35 @@ import { Component, Input } from '@angular/core';
 import { NgClass, NgOptimizedImage } from '@angular/common';
 import { TSize } from '../../types/common.type';
 
+export type TImageProps = {
+  src: string;
+  xSize: TSize;
+  ySize: TSize;
+  alt: string;
+};
+
 @Component({
   selector: 'app-image',
   standalone: true,
   imports: [NgOptimizedImage, NgClass],
   template: `
     <img
-      [ngSrc]="src"
+      [ngSrc]="props.src"
       width="1"
       height="1"
-      [alt]="alt"
+      [alt]="props.alt"
       [ngClass]="{
-        'w-6': xSize === 'sm',
-        'w-24': xSize === 'md',
-        'w-48': xSize === 'lg',
-        'w-96': xSize === 'xlg',
-        'h-6': ySize === 'sm',
-        'h-24': ySize === 'md',
-        'h-48': ySize === 'lg',
-        'h-96': ySize === 'xlg',
+        'w-6': props.xSize === 'sm',
+        'w-24': props.xSize === 'md',
+        'w-48': props.xSize === 'lg',
+        'w-96': props.xSize === 'xlg',
+        'h-6': props.ySize === 'sm',
+        'h-24': props.ySize === 'md',
+        'h-48': props.ySize === 'lg',
+        'h-96': props.ySize === 'xlg',
       }" />
   `,
 })
 export class ImageComponent {
-  @Input({ required: true }) src = '';
-  @Input({ required: true }) xSize: TSize = 'sm';
-  @Input({ required: true }) ySize: TSize = 'sm';
-  @Input() alt = '';
+  @Input({ required: true }) props!: TImageProps;
 }
