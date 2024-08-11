f96c1df5c8f8940b509c61a6703245254ddaca8a
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-11 19:00:17 -0300

    chore: code format

--- frontend/src/app/pages/home/components/footer/footer.component.ts
+++ frontend/src/app/pages/home/components/footer/footer.component.ts
@@ -5,6 +5,6 @@ import { NgClass } from '@angular/common';
   selector: 'home-footer',
   standalone: true,
   imports: [NgClass],
-  template: ` <footer class="h-20 w-full bg-secondary"></footer> `,
+  template: ` <footer class="h-20 bg-secondary"></footer> `,
 })
 export class FooterComponent {}
