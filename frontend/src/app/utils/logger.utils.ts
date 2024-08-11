7ef45c6f15cec6fe9450ec0a3682094ef00acb70
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-11 19:12:20 -0300

    chore: add comment for the logger util

--- frontend/src/app/utils/logger.utils.ts
+++ frontend/src/app/utils/logger.utils.ts
@@ -1,5 +1,8 @@
 import { environment } from '../../environments/environment';
 
+//Should NOT be used inside a component
+//
+//If it is needed, then the loggerService should be used instead
 export const logger = {
   debug(message: any) {
     if (environment.PRODUCTION) return;
