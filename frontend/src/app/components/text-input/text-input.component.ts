5562bb582aff0acde083c78240c6a9f1b5db6d48
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-08-11 19:15:56 -0300

    feat!: modify text-input component to use prop type

--- frontend/src/app/components/text-input/text-input.component.ts
+++ frontend/src/app/components/text-input/text-input.component.ts
@@ -2,43 +2,47 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { NgClass } from '@angular/common';
 
+export type TTextInputProps = {
+  label: string;
+  placeholder: string;
+  formControl: FormControl;
+  invalidFormLabel: string;
+};
+
 @Component({
   selector: 'app-text-input',
   standalone: true,
   imports: [ReactiveFormsModule, NgClass],
   template: `
-    <label class="block font-bold text-secondary" [for]="label">
-      {{ label }}
+    <label class="block font-bold text-secondary" [for]="props.label">
+      {{ props.label }}
     </label>
     <div class="h-18">
       <input
-        [id]="label"
+        [id]="props.label"
         class="w-full rounded border-2 bg-primary px-4 py-2 outline-none transition-colors duration-300"
         [ngClass]="isFormValid ? 'border-secondary' : 'border-error'"
         type="text"
-        [formControl]="ngxFormControl"
-        [placeholder]="placeholder" />
+        [formControl]="props.formControl"
+        [placeholder]="props.placeholder" />
 
       <label
         class="w-full font-medium text-error transition-opacity duration-300"
         [ngClass]="isFormValid ? 'invisible opacity-0' : 'visible'"
-        [for]="label">
-        *{{ invalidFormLabel }}
+        [for]="props.label">
+        *{{ props.invalidFormLabel }}
       </label>
     </div>
   `,
 })
 export class TextInputComponent implements OnInit {
-  @Input({ required: true }) label = '';
-  @Input({ required: true }) placeholder = '';
-  @Input({ required: true }) ngxFormControl = new FormControl('');
-  @Input({ required: true }) invalidFormLabel = '';
+  @Input({ required: true }) props!: TTextInputProps;
 
   isFormValid = true;
   ngOnInit() {
-    this.ngxFormControl.statusChanges.subscribe(_ => {
+    this.props.formControl.statusChanges.subscribe(_ => {
       this.isFormValid =
-        this.ngxFormControl.pristine || this.ngxFormControl.valid;
+        this.props.formControl.pristine || this.props.formControl.valid;
     });
   }
 }
