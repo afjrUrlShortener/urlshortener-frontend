70d6509f5f8a05165580138536a26451a0047cfc
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-09-27 10:16:48 -0300

    feat: add typography component to link

--- frontend/src/app/components/link/link.component.ts
+++ frontend/src/app/components/link/link.component.ts
@@ -1,31 +1,59 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IconComponent } from '../icon/icon.component';
-import { TIconName } from '../../types/common.type';
+import { IconComponent, TIconProps } from '../icon/icon.component';
+import { NgClass } from '@angular/common';
+import { TColor } from '../../types/common.type';
+import {
+  TTypographyProps,
+  TypographyComponent,
+} from '../typography/typography.component';
+
+export type TLinkProps = {
+  link: string;
+  typography?: TTypographyProps;
+  icon?: TIconProps;
+  hasUnderline: boolean;
+  hovered?: boolean;
+  underlineColor?: TColor;
+};
 
 @Component({
   selector: 'app-link',
   standalone: true,
-  imports: [RouterLink, IconComponent],
+  imports: [RouterLink, IconComponent, NgClass, TypographyComponent],
   template: `
-    <a class="group flex flex-col" [routerLink]="link">
-      <div class="flex items-center justify-center">
-        @if (text && iconName) {
-          <span class="font-medium">{{ text }}</span>
-          <app-icon class="ml-2 mt-1" [name]="iconName" size="xsm" />
-        } @else if (text) {
-          <span class="font-medium">{{ text }}</span>
-        } @else if (iconName) {
-          <app-icon [name]="iconName" />
+    <a
+      class="flex flex-col"
+      [routerLink]="props.link"
+      (mouseenter)="mouseenter.emit(props)"
+      (mouseleave)="mouseleave.emit(props)">
+      <div class="mb-2 flex items-center justify-center">
+        @if (props.typography && props.icon) {
+          <app-typography [props]="props.typography" />
+          <app-icon class="ml-2 mt-1" [props]="props.icon" />
+        } @else if (props.typography) {
+          <app-typography [props]="props.typography" />
+        } @else if (props.icon) {
+          <app-icon [props]="props.icon" />
         }
       </div>
-      <div
-        class="mt-2 w-0 rounded transition-all duration-300 group-hover:w-full group-hover:outline group-hover:outline-1"></div>
+
+      @if (props.hasUnderline) {
+        <div
+          class="rounded outline outline-1 duration-300"
+          [ngClass]="{
+            'outline-primary': props.underlineColor === 'primary',
+            'outline-secondary': props.underlineColor === 'secondary',
+            'outline-tertiary': props.underlineColor === 'tertiary',
+            'w-0 outline-transparent transition-width': !props.hovered,
+            'w-full transition-width': props.hovered,
+          }"></div>
+      }
     </a>
   `,
 })
 export class LinkComponent {
-  @Input({ required: true }) link = '';
-  @Input() text?: string;
-  @Input() iconName?: TIconName;
+  @Input({ required: true }) props!: TLinkProps;
+  mouseenter = output<TLinkProps>();
+  mouseleave = output<TLinkProps>();
 }
